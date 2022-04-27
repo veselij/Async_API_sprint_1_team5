fbc5c4567c60b68a3d70ab44da88350c3259fd52
Aleksandr Minaev <veselij85@gmail.com>
2022-04-27 23:50:10 +0300

    added test infra

---
+++ fastapi-solution/tests/functional/utils/wait_for_es.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from time import sleep
+
+from elasticsearch import Elasticsearch
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from settings import TestSettings
+
+
+def check_es_ready():
+    config = TestSettings()
+    es = Elasticsearch(['http://{0}:{1}/'.format(config.es_host, config.es_port)])
+    while not es.ping():
+        sleep(1)
+
+
+if __name__ == '__main__':
+    check_es_ready()

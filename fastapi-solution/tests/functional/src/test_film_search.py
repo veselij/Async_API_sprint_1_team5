a043110528f6e0ea8a41bfb29290ea0ad89dd674
Aleksandr Minaev <veselij85@gmail.com>
2022-04-29 14:35:09 +0300

    added more search tests

--- fastapi-solution/tests/functional/src/test_film_search.py
+++ fastapi-solution/tests/functional/src/test_film_search.py
@@ -21,12 +21,38 @@ async def test_films_search_empty(make_get_request):
 
 
 @pytest.mark.asyncio
-async def test_films_search(make_get_request, prepare_es_index, populate_index):
+async def test_films_search_one(make_get_request, prepare_es_index, populate_index):
 
     await prepare_es_index('testdata/movies.json')
     await populate_index('testdata/movies_data.json')
 
-    response = await make_get_request('http://{0}:8000/api/v1/films/search/?query=Star'.format(config.api_ip))
+    response = await make_get_request('http://{0}:8000/api/v1/films/search/?query=Star&page_num=1&page_size=1'.format(config.api_ip))
 
     assert response.status == 200
     assert response.body[0]['uuid'] == '05d7341e-e367-4e2e-acf5-4652a8435f93'
+
+
+@pytest.mark.asyncio
+async def test_films_search_two(make_get_request, prepare_es_index, populate_index):
+
+    await prepare_es_index('testdata/movies.json')
+    await populate_index('testdata/movies_data.json')
+
+    response = await make_get_request('http://{0}:8000/api/v1/films/search/?query=Star&page_num=1&page_size=2'.format(config.api_ip))
+
+    assert response.status == 200
+    assert len(response.body) == 2
+
+
+@pytest.mark.asyncio
+async def test_films_search_datatypes(make_get_request, prepare_es_index, populate_index):
+
+    await prepare_es_index('testdata/movies.json')
+    await populate_index('testdata/movies_data.json')
+
+    response = await make_get_request('http://{0}:8000/api/v1/films/search/?query=Star&page_num=1&page_size=1'.format(config.api_ip))
+
+    assert response.status == 200
+    assert isinstance(response.body[0]['uuid'], str)
+    assert isinstance(response.body[0]['title'], str)
+    assert isinstance(response.body[0]['imdb_rating'], int)

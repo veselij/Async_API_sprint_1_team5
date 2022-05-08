26e1eb9015d10099665f83d8d5bf11dcd9ff9846
Amirbeg Kurbanov <avastfsoc541@gmail.com>
2022-05-08 09:44:21 +0300

    Test genre

---
+++ fastapi-solution/tests/functional/src/test_genres.py
@@ -0,0 +1,101 @@
+import pytest
+
+from settings import config
+from testdata.genres import genre_pagination_wrong_params, genre_pagination_test_data, genre_info_by_uuid_test
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('params,results', genre_pagination_wrong_params)
+async def test_genre_pagination_wrong_params(params, results, make_get_request, clear_redis):
+
+    response = make_get_request(
+        f'http://{config.api_ip}:8000/api/v1/genres/{params}'
+    )
+
+    assert response.status == 422
+    assert response.body == results
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('page_num,page_size,results_len,status_code', genre_pagination_test_data)
+async def test_genres(
+    page_num,
+    page_size,
+    results_len,
+    status_code,
+    prepare_es_index,
+    populate_index,
+    make_get_request,
+    clear_redis,
+    ):
+
+    await prepare_es_index("testdata/genres.json")
+    await populate_index("testdata/genres_data.json")
+
+    response = make_get_request(
+        f'http://{config.api_ip}:8000/api/v1/genres/?page_num={page_num}&page_size={page_size}'
+    )
+
+    assert response.status == status_code
+    assert len(response.body) == results_len
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('uuid,result', genre_info_by_uuid_test)
+async def test_genre_by_uuid(
+    uuid,
+    result,
+    make_get_request,
+    prepare_es_index,
+    populate_index,
+    clear_redis,
+    ):
+
+    await prepare_es_index("testdata/genres.json")
+    await populate_index("testdata/genres_data.json")
+
+    response = make_get_request(
+        f'http://{config.api_ip}:8000/api/v1/genres/{uuid}'
+    )
+
+    assert response.status == 200
+    assert len(response.body) == result
+
+@pytest.mark.asyncio
+async def test_genre_cashe(
+    make_get_request,
+    prepare_es_index,
+    populate_index,
+    es_client,
+    clear_redis,
+    ):
+
+    await prepare_es_index("testdata/genres.json")
+    await populate_index("testdata/genres_data.json")
+
+    response = make_get_request(
+        f'http://{config.api_ip}:8000/api/v1/genres/3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff'
+    )
+
+    assert response.status == 200
+    assert len(response.body) == [{
+        "uuid": "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff",
+        "name": "Action",
+        "description": ""
+    }]
+
+
+    await es_client.options(ignore_status=[404]).indices.delete(index="movies")
+    assert not await es_client.indices.exists(index="movies")
+
+    response = make_get_request(
+        f'http://{config.api_ip}:8000/api/v1/genres/3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff'
+    )
+
+    assert response.status == 200
+    assert len(response.body) == [{
+        "uuid": "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff",
+        "name": "Action",
+        "description": ""
+    }]
+
+
---
+++ fastapi-solution/tests/functional/testdata/genres.py
@@ -0,0 +1,23 @@
+import json
+
+genre_pagination_wrong_params = {
+    ("?page_num=-1&page_size=1", {"detail":[{"loc":["query","page_num"],"msg":"ensure this value is greater than or equal to 1","type":"value_error.number.not_ge","ctx":{"limit_value":1}}]}),
+    ("?page_num=1&page_size=-1", {"detail":[{"loc":["query","page_size"],"msg":"ensure this value is greater than or equal to 1","type":"value_error.number.not_ge","ctx":{"limit_value":1}}]}),
+}
+
+
+genre_test_data = [
+    (1, 1, 1, 200),
+    (1, 2, 2, 200),
+    (1, 10, 10, 200),
+]
+
+with open('testdata/genres_data.json', 'r') as f:
+            data = json.load(f)
+
+genre_info_by_uuid_test = [
+    ('2a090dde-f688-46fe-a9f4-b781a985275e', data[0]),
+    ('05d7341e-e367-4e2e-acf5-4652a8435f93', data[1]),
+    ('15d7341e-e367-4e2e-acf5-4652a8435f93', data[2]),
+]
+

aab222676116fa52fdef06e44ef3ca2db8b93139
Aleksandr Minaev <veselij85@gmail.com>
2022-05-31 00:21:54 +0300

    add auth url config

--- fastapi-solution/src/auth/authorization.py
+++ fastapi-solution/src/auth/authorization.py
@@ -1,4 +1,5 @@
 from fastapi.security.http import HTTPBearer
+from orjson import JSONDecodeError
 from starlette.requests import Request
 import httpx
 from core import config
@@ -19,6 +20,9 @@ class TokenCheck(HTTPBearer):
 
     async def send_request_to_auth(self, token: str) -> list:
         async with httpx.AsyncClient() as client:
-            response = await client.post(f"http://{config.AUTH_HOST}:{config.AUTH_PORT}/user/check", json={"access_token": token})
-            print(response)
-        return response.json()
+            response = await client.post(config.AUTH_URL, json={"access_token": token})
+            try:
+                result = response.json()
+            except JSONDecodeError:
+                result = []
+        return result
--- fastapi-solution/src/core/config.py
+++ fastapi-solution/src/core/config.py
@@ -13,7 +13,6 @@ REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 ELASTIC_HOST = os.getenv('ELASTIC_HOST', '127.0.0.1')
 ELASTIC_PORT = int(os.getenv('ELASTIC_PORT', 9200))
 
-AUTH_HOST = os.getenv('AUTH_HOST', '127.0.0.1')
-AUTH_PORT = int(os.getenv('AUTH_PORT', 8000))
+AUTH_URL = os.getenv('AUTH_URL', 'http://127.0.0.1:8001/api/v1/roles/user/check')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
